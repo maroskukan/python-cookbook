dd641351ad91ea5b82a69a2af2441440f42a38ca
Maros Kukan <maros.kukan@me.com>
2021-11-30 14:01:50 +0100

    add global vs local variable scope, add argument examples

--- ingredients/functions.py
+++ ingredients/functions.py
@@ -44,3 +44,45 @@ print(power(2, 3))
 print(power(x=3, num=2))
 
 print(multi_add(1, 2, 3, 4, 5))
+
+# define a global variable
+cheese = "cheddar"
+
+# create a generic mix and cook function
+def mix_and_cook():
+    print("Mixing the ingredients")
+    print("Pouring the mixture into a frying pan")
+    print("Cooking the omelette for 5 minutes")
+    print("Enjoy!")
+
+
+# leverage existing function and global variable if local is not defined
+def make_omelete():
+    mix_and_cook()
+    omellete = f"A teasty {cheese} Omelette"
+    return omellete
+
+
+# manimulate a global variable from function
+def make_pancake():
+    mix_and_cook()
+    global cheese
+    cheese = "swiss"
+    pancake = f"A delicious {cheese} pancake"
+    return pancake
+
+
+# create a function that takes variable number of arguments
+def fancy_omelette(*ingredients):
+    mix_and_cook()
+    omellete = f"A fancy omelette with {len(ingredients)} ingredients"
+    return omellete
+
+
+omelette2 = fancy_omelette("cheese", "tomato", "egg")
+pancake = make_pancake()
+omelette1 = make_omelete()
+
+print(omelette1)
+print(omelette2)
+print(pancake)
