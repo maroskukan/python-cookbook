3dd5baf34829f483ab3a2a4ce21095c7abcf5b0f
Maros Kukan <maros.kukan@me.com>
2021-12-07 15:16:11 +0100

    Add sets examples

--- ingredients/variables.py
+++ ingredients/variables.py
@@ -57,3 +57,90 @@ for floor in lot_3d:
         for car in row:
             print(car)
 
+# Creating and Combining Sets of Friends
+
+collage = set(["Bill", "Katy", "Verne", "Dillon" "Bruce", "Olivia", "Richard", "Jim"])
+coworkers = set(
+    ["Aaron", "Bill", "Brandon", "David", "Frank", "Connie", "Kyle", "Olivia"]
+)
+family = set(["Garry", "Landon", "Larry", "Mark", "Olivia", "Katy", "Rae", "Tom"])
+
+# Count objects in a set
+noColl = len(collage)
+noCowo = len(coworkers)
+noFami = len(family)
+
+# Single set of all friends
+friends = collage.union(coworkers, family)
+
+# Sorting Friends into Sets
+
+# set of all friends
+friends = set(
+    [
+        "Mark",
+        "Rae",
+        "Verne",
+        "Richard",
+        "Aaron",
+        "David",
+        "Bruce",
+        "Garry",
+        "Bill",
+        "Connie",
+        "Larry",
+        "Jim",
+        "Landon",
+        "Dillon",
+        "Frank",
+        "Tom",
+        "Kyle",
+        "Katy",
+        "Olivia",
+        "Brandon",
+    ]
+)
+
+# set of people who live in my zip code
+zipcode = set(
+    [
+        "Jerry",
+        "Eleine",
+        "Cindy",
+        "Verne",
+        "Rudolph",
+        "Bill",
+        "Olivia",
+        "Jim",
+        "Lindsay",
+        "Rae",
+        "Mark",
+        "Kramer",
+        "Landon",
+        "Newman",
+        "George",
+    ]
+)
+
+# set of people who play Munchkin
+munchkins = set(["Steve", "Jackson", "Frank", "Bill", "Mark", "Landon", "Rae"])
+
+# set of Olivia's friends
+olivia = set(["Jim", "Amanda", "Verne", "Nestor"])
+
+local = friends.intersection(zipcode)
+invite = local.difference(munchkins)
+munchkins.difference(local)
+invite = invite.symmetric_difference(olivia)
+
+# Check if item exists in set
+verne_present = "Verne" in invite
+
+# Add item to a set
+invite.add("Verne")
+
+# Remove item from set
+invite.remove("Nestor")
+
+# Return item form set
+removed = invite.pop()
