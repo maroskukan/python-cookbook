928736106de677daf7411f5a08ab9a5495c3b98a
Maros Kukan <maros.kukan@me.com>
2021-12-09 20:01:51 +0100

    Add simple if-elif-else and switch-case stamenets using dictionary

--- ingredients/conditionals.py
+++ ingredients/conditionals.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from datetime import date
+import calendar
+
 
 def main():
     x, y = 10, 100
@@ -14,6 +17,39 @@ def main():
     # conditional statements let you use a "a if C else b"
     st = "x is less than y" if (x < y) else "x is greater or same as y"
     print(st)
+
+    # ordering a pizza example
+    diet_restrictions = set(["meat", "cheese"])
+
+    # Cheese pizza will be selected
+    # in order to fix it more specific conditions should be on top
+    if "meat" in diet_restrictions:
+        print("Get a chesse pizza.")
+    elif "meat" and "cheese" in diet_restrictions:
+        print("Get a vegan pizza.")
+    else:
+        print("Get something else.")
+
+    # Simulate switch and case statements
+    specials = {
+        "Monday": "spinach",
+        "Tuesday": "mushrooms",
+        "Wednesday": "tomatoes",
+        "Thursday": "cheese",
+        "Friday": "pepperoni",
+        "Saturday": "chicken",
+        "Sunday": "eggs",
+    }
+
+    def order(day):
+        if day in specials:
+            print("Today's special is {}".format(specials[day]))
+        else:
+            print("No special today.")
+
+    # Get the name of the day and used that as an argument
+    today = calendar.day_name[date.today().weekday()]
+    order(today)
 
 
 if __name__ == "__main__":
