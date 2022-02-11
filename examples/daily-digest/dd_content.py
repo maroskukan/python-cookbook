4440b519449b55160d417c7d8747f7e0bc2b6caa
Maros Kukan <maros.kukan@me.com>
2022-02-11 11:19:15 +0100

    Add get_random_quote function with sample local qoute content file

--- examples/daily-digest/dd_content.py
+++ examples/daily-digest/dd_content.py
@@ -1,5 +1,25 @@
-def get_random_quote():
-    pass
+import csv
+import random
+
+
+"""
+Retrieve a random quote from the specifid CSV file.
+"""
+
+
+def get_random_quote(quotes_file="quotes.csv"):
+    try:  # load motivational quotes from csv file
+        with open(quotes_file) as csvfile:
+            quotes = [
+                {"author": line[0], "quote": line[1]}
+                for line in csv.reader(csvfile, delimiter="|")
+            ]
+    except Exception as e:  # use a default quote to help things to turn out for the best
+        quotes = [
+            {"author": "Eric Idle", "quote": "Always Look on the Bright Side of Life."}
+        ]
+
+    return random.choice(quotes)
 
 
 def get_weather_forecast():
@@ -15,4 +35,11 @@ def get_wikipedia_article():
 
 
 if __name__ == "__main__":
-    pass  # test code
+    ##### test get_random_quote() #####
+    print(("\nTesting quote generation..."))
+
+    quote = get_random_quote()
+    print(f' - Random quote is "{quote["quote"]}" by {quote["author"]}')
+
+    quote = get_random_quote(quotes_file=None)
+    print(f' - Default quote is "{quote["quote"]}" by {quote["author"]}')
