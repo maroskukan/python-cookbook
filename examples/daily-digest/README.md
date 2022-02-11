ffc81be1602c59505fec2d232c09630be9356b02
Maros Kukan <maros.kukan@me.com>
2022-02-11 14:40:29 +0100

    Add get_weather_forecast function and describe required env vars

---
+++ examples/daily-digest/README.md
@@ -0,0 +1,12 @@
+# Daily Digest Application
+
+## External Content Retrieval
+
+### Weather data
+
+In order to retrieve data from [openweathermap](https://openweathermap.org), you need to make sure the following system variable exist and is valid.
+
+```bash
+# Example credentials
+export OW_API_KEY=3eabc245650b64b87867afc08e5d1bac
+```
--- examples/daily-digest/dd_content.py
+++ examples/daily-digest/dd_content.py
@@ -1,5 +1,9 @@
+import datetime
 import csv
 import random
+from urllib import request
+import json
+import os
 
 
 """
@@ -22,8 +26,40 @@ def get_random_quote(quotes_file="quotes.csv"):
     return random.choice(quotes)
 
 
-def get_weather_forecast():
-    pass
+"""
+Retrieve the current weather forecast from OpenWeatherMap.
+"""
+
+
+def get_weather_forecast(
+    coords={"lat": 28.4717, "lon": -80.5378}
+):  # default location at Cape Canaveral
+    try:  # retrieve forecast for specified coordinates
+        OW_API_KEY = os.environ.get(
+            "OW_API_KEY"
+        )  # load OpenWeatherMap API key from environment variable
+        url = f'https://api.openweathermap.org/data/2.5/forecast?lat={coords["lat"]}&lon={coords["lon"]}&units=metric&appid={OW_API_KEY}'
+        data = json.load(request.urlopen(url))
+
+        forecast = {
+            "city": data["city"]["name"],  # city name
+            "country": data["city"]["country"],  # country name
+            "periods": list(),  # list to hold forecast data for future periods
+        }
+
+        for period in data["list"][0:9]:  # pupulate list with next 9 forecast periods
+            forecast["periods"].append(
+                {
+                    "timestamp": datetime.datetime.fromtimestamp(period["dt"]),
+                    "temp": round(period["main"]["temp"]),
+                    "description": period["weather"][0]["description"].title(),
+                    "icon": f'http//openweathermap.org/img/wm/{period["weather"][0]["icon"]}.png',
+                }
+            )
+        return forecast
+
+    except Exception as e:
+        print(f"\nWeather forecast not available.")
 
 
 def get_twitter_trends():
@@ -43,3 +79,28 @@ if __name__ == "__main__":
 
     quote = get_random_quote(quotes_file=None)
     print(f' - Default quote is "{quote["quote"]}" by {quote["author"]}')
+
+    ##### test get_weather_forecast() #####
+    print(("\nTesting weather forecast retrieval..."))
+
+    forecast = get_weather_forecast()  # get forecast for default location
+    if forecast:
+        print(f'nWeather forecast for {forecast["city"]}, {forecast["country"]} is...')
+        for period in forecast["periods"]:
+            print(
+                f' - {period["timestamp"]} | {period["temp"]}°C | {period["description"]}'
+            )
+
+    slovakia = { "lat": 49.1666, "lon": 20.1316, }  # coordinates for Novy Smokovec, Slovakia
+    forecast = get_weather_forecast( coords = slovakia )  # get forecast for Novy Smokovec, Slovakia
+    if forecast:
+        print(f'\nWeather forecast for {forecast["city"]}, {forecast["country"]} is...')
+        for period in forecast["periods"]:
+            print(
+                f' - {period["timestamp"]} | {period["temp"]}°C | {period["description"]}'
+            )
+
+    invalid = {"lat": 1234.5678, "lon": 1234.5678}  # invalid coordinates
+    forecast = get_weather_forecast( coords = invalid)  # get forecast for invalid coordinates
+    if forecast is None:
+        print("Weather forecast for invalid coordinates returned None")
